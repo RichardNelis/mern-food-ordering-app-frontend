1a445282346c29127bab5375b40e68d07a19fa49
Richard Nelis de Souza <souza.richard33@hotmail.com>
2024-03-17 22:58:25 -0300

    Alterado o tamanho do texto do footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -4,13 +4,13 @@ export const Footer = () => {
   return (
     <div className="bg-orange-500 py-10">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <span className="text-3xl text-white font-bold tracking-tight">
+        <span className="text-xl text-white font-bold tracking-tight">
           MernEats.com
         </span>
         <Separator className="md:hidden w-60" />
         <span className="text-white font-bold tracking-tight flex gap-4">
-          <span>Privacy Policy</span>
-          <span>Terms of Service</span>
+          <span className="text-xs md:text-base">Privacy Policy</span>
+          <span className="text-xs md:text-base">Terms of Service</span>
         </span>
       </div>
     </div>
