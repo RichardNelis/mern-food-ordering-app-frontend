eb86de0cef6d6b1519e70b7bac7e7ac0c10e0da4
Richard Nelis de Souza <souza.richard33@hotmail.com>
2024-03-18 23:30:54 -0300

    Adicionado o componente search

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,26 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+import { useParams } from "react-router-dom";
+
+export const SearchPage = () => {
+  const { city } = useParams();
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
+
+  return (
+    <span>
+      User searched for {city}{" "}
+      {results?.data.map((restaurant) => (
+        <span>
+          found - {restaurant.restaurantName}, {restaurant.city}
+        </span>
+      ))}
+    </span>
+  );
+};
